a521d6b57dcffea9ffacfa79eb768f8aad8c906c
josephhadad05 <91989021+josephhadad05@users.noreply.github.com>
2022-10-17 12:11:20 -0400

    Better Upload

--- better.py
+++ better.py
@@ -1,38 +1,24 @@
-import cv2
-import numpy as np
+import numpy
+import tensorflow as tf
 
-import tensorflow.lite as tflite
-from PIL import Image
+keras_model = tf.keras.applications.MobileNetV2()
 
+def preprocessImage(path):
+    img = tf.keras.preprocessing.image.load_img(path, target_size=(224, 224))
+    imgArray = tf.keras.preprocessing.image.img_to_array(img)
+    imgArrayExpand = numpy.expand_dims(imgArray, axis=0)
+    return tf.keras.applications.mobilenet.preprocess_input(imgArrayExpand)
 
-interpreter: tflite.Interpreter = tflite.Interpreter(
-    r"lite-model_yolo-v5-tflite_tflite_model_1.tflite"
-)
-interpreter.allocate_tensors()
+def predictImage(model, preprocessedImage):
+    prediction = model.predict(preprocessedImage)
+    results = tf.keras.applications.imagenet_utils.decode_predictions(prediction)[0]
+    classification = ("Unknown Quantity", 0)
+    for result in results:
+        if result[2] > classification[1] and result[2] > 0.1:
+            classification = (result[1], result[2])
+    return classification[0]
 
-input_details = interpreter.get_input_details()
-input_index = input_details[0]["index"]
-input_shape = input_details[0]["shape"]
-height = input_shape[1]
-width = input_shape[2]
+imagePath = input("Enter absolute path to image: ")
+preprocessedImage = preprocessImage(imagePath)
 
-
-cap = cv2.VideoCapture(0)
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-
-    image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)).resize(
-        (width, height)
-    )
-
-    input_data = np.expand_dims(image, axis=0).astype(np.float32)  # expand to 4-dim
-
-    # Process
-    interpreter.set_tensor(input_index, input_data)
-    interpreter.invoke()
-
-    output_details = interpreter.get_output_details()
-    output_data = interpreter.get_tensor(output_details[0]["index"])[0]
-    print(output_data)
-    break
+print(f"This is an image of a {predictImage(keras_model, preprocessedImage)}.")
