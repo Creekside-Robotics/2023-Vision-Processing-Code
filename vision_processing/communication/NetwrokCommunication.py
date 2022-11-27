de5d330fe6eeadee36945c4a292c407707fd989f
GarfieldArbuckle <mcyel25@gmail.com>
2022-11-26 23:38:04 -0500

    Created Networkable Communications File

--- vision_processing/communication/NetwrokCommunication.py
+++ vision_processing/communication/NetwrokCommunication.py
@@ -6,3 +6,12 @@ class Networktables:
     def __init__(self):
         self.ntinst = networktables.NetworkTablesInstance.getDefault()
         self.ntinst.startClientTeam(8775) 
+        
+    def SETaprilTagPoseDataEntry(self, poseArray): 
+        self.ntinst.getEntry("April Tag Pose Entry").setDoubleArray(poseArray)
+    def GETkinematicsDataEntry(self, poseArray):
+        self.ntinst.getEntry("Kinematics Data Entry").setDoubleArray(poseArray)
+    def GETfusedPoseDataEntry(self, poseArray):
+        self.ntinst.getEntry("Fused Pose Data Entry").setDoubleArray(poseArray)
+    
+    
