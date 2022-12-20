77899ff73be2905d77813e9e37e867ca4b34510a
josephhadad05 <91989021+josephhadad05@users.noreply.github.com>
2022-12-19 23:09:58 -0500

    Added main testing script, test.py

---
+++ test.py
@@ -0,0 +1,44 @@
+import time
+
+from testing import TestImageCommunications, TestNetworkCommunication
+from vision_processing import GameField, Robot, DynamicObjectProcessing, DynamicField, \
+    FieldProcessing, ReferencePoint, UserInterface
+
+cameras = GameField.cameras
+object_detection = DynamicObjectProcessing()
+
+communications = TestNetworkCommunication()
+image_communications = TestImageCommunications("HyperClock UI")
+robot = Robot()
+
+field = DynamicField(robot, communications)
+field_processor = FieldProcessing(field)
+user_interface = UserInterface(field_processor)
+
+while True:
+    dynamic_objects = []
+    reference_points = []
+    timestamp = time.time()
+
+    # Processing frames
+    for camera in cameras:
+        dynamic_objects.extend(object_detection.get_dynamic_objects(camera))
+        reference_points.extend(ReferencePoint.from_apriltags(camera))
+
+    # Updating field
+    field.update_field(timestamp, reference_points, dynamic_objects)
+
+    # Processing field and getting output
+    field_processor.process_field()
+    output = field_processor.get_output()
+
+    # Sending output to robot
+    communications.set_robot_output(output)
+
+    # Generating and sending user interface
+    interface_frame = user_interface.generate_field()
+    image_communications.put_frame(interface_frame)
+
+    # Printing FPS
+    fps = 1 / (time.time() - timestamp)
+    print(f"Cycle was successful.\nFPS: {round(fps, 3)}")
