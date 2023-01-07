69d1a56067cf48bc8fa0843b48c9754362585d72
josephhadad05 <91989021+josephhadad05@users.noreply.github.com>
2023-01-07 13:40:24 -0500

    Automatic Capture

--- vision_processing/communication/image_communications.py
+++ vision_processing/communication/image_communications.py
@@ -9,6 +9,7 @@ class ImageCommunications:
         @param width: Width of camera stream, defaults to 1920
         @param height: Height of the camera stream, defaults to 1080
         """
+        CameraServer.startAutomaticCapture()
         self.source: CvSource = CameraServer.putVideo(name, width, height)
 
     def put_frame(self, frame):
