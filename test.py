a7087e27ddb157d90b9fcfb4831160b7097f70cf
josephhadad05 <91989021+josephhadad05@users.noreply.github.com>
2023-01-19 22:28:45 -0500

    Trimming down project

--- test.py
+++ test.py
@@ -17,14 +17,6 @@ cameras = [
 object_detection = vision_processing.DynamicObjectProcessing()
 
 communications = testing.TestNetworkCommunication()
-image_communications = testing.TestImageCommunications("HyperClock UI")
-fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-writer = cv2.VideoWriter('testing/testing_resources/HyperClock Output Video.mp4', fourcc, 30, (350, 500))
-robot = vision_processing.Robot()
-
-field = vision_processing.DynamicField(robot, communications)
-field_processor = vision_processing.FieldProcessing(field)
-user_interface = vision_processing.UserInterface(field_processor)
 
 while True:
     dynamic_objects = []
@@ -36,20 +28,6 @@ while True:
         camera.update_frame()
         dynamic_objects.extend(object_detection.get_dynamic_objects(camera))
         reference_points.extend(vision_processing.ReferencePoint.from_apriltags(camera))
-
-    # Updating field
-    field.update_field(cameras[0].frame_time, reference_points, dynamic_objects)
-
-    # Processing field and getting output
-    field_processor.process_field()
-    output = field_processor.get_output()
-
-    # Sending output to robot
-    communications.set_robot_output(output)
-
-    # Generating and sending user interface
-    interface_frame = user_interface.generate_field()
-    writer.write(interface_frame)
 
     # Printing FPS
     fps = 1 / (time.time() - timestamp)
--- vision_processing/__init__.py
+++ vision_processing/__init__.py
@@ -1,6 +1,4 @@
 from .utils import *
 from .constants import *
 from .vision import *
-from .communication import NetworkCommunication, ImageCommunications
-from .decision_making import *
-from .communication import UserInterface
+from .communication import NetworkCommunication
