048d58446b36fa88308045482ddd86e90c083ce5
josephhadad05 <91989021+josephhadad05@users.noreply.github.com>
2023-02-09 20:28:07 -0500

    Refactored main scripts

--- main.py
+++ main.py
@@ -1,29 +1,4 @@
-import time
-from operator import attrgetter
+from run import PipelineRunner
 
-import vision_processing
-
-cameras = [vision_processing.Camera.from_list(camera) for camera in vision_processing.GameField.cameras]
-object_detection = vision_processing.DynamicObjectProcessing()
-
-communications = vision_processing.NetworkCommunication()
-
-while True:
-    dynamic_objects = []
-    reference_points = []
-    timestamp = time.time()
-
-    # Processing frames
-    for camera in cameras:
-        camera.update_frame()
-        dynamic_objects.extend(object_detection.get_dynamic_objects(camera))
-        reference_points.extend(vision_processing.ReferencePoint.from_apriltags(camera))
-
-    if reference_points:
-        communications.send_pose(max(reference_points, key=attrgetter("decision_margin")).robot_pose)
-
-    communications.send_objects(dynamic_objects)
-
-    # Printing FPS
-    fps = 1 / (time.time() - timestamp)
-    print(f"Cycle was successful.\nFPS: {round(fps, 3)}")
+pipeline = PipelineRunner()
+pipeline.run()
--- test.py
+++ test.py
@@ -1,38 +1,4 @@
-import time
-import math
-from operator import attrgetter
+from run import PipelineRunner
 
-import vision_processing
-import testing
-
-cameras = [
-    vision_processing.Camera(
-        (0.106, 0, 0.6606),
-        (0, -math.pi / 7.5),
-        650,
-        "testing/testing_resources/HyperClock Test Video.mp4"
-    )
-]
-
-object_detection = vision_processing.DynamicObjectProcessing()
-communications = testing.TestNetworkCommunication()
-
-while True:
-    dynamic_objects = []
-    reference_points = []
-    timestamp = time.time()
-
-    # Processing frames
-    for camera in cameras:
-        camera.update_frame()
-        dynamic_objects.extend(object_detection.get_dynamic_objects(camera))
-        reference_points.extend(vision_processing.ReferencePoint.from_apriltags(camera))
-
-    if reference_points:
-        communications.send_pose(max(reference_points, key=attrgetter("decision_margin")).robot_pose)
-
-    communications.send_objects(dynamic_objects)
-
-    # Printing FPS
-    fps = 1 / (time.time() - timestamp)
-    print(f"Cycle was successful.\nFPS: {round(fps, 3)}")
+pipeline = PipelineRunner(test=True)
+pipeline.run()
