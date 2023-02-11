3634c4b3ac62d196f702eefccad84f1ab1058e9b
josephhadad05 <91989021+josephhadad05@users.noreply.github.com>
2023-02-10 19:50:44 -0500

    Seperated testing from main code

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
+import testing
+import vision_processing
+
 from run import PipelineRunner
 
-pipeline = PipelineRunner(test=True)
+pipeline = PipelineRunner(
+    communications=testing.TestNetworkCommunication(),
+    cameras=[vision_processing.Camera.from_list(vision_processing.GameField.test_camera)]
+)
 pipeline.run()
