42795f776a65b645d86ebb4595b21948c8f0a36f
josephhadad05 <91989021+josephhadad05@users.noreply.github.com>
2023-02-10 19:58:22 -0500

    Updating test_network_communinications.py

--- testing/test_network_communinications.py
+++ testing/test_network_communinications.py
@@ -1,58 +1,20 @@
-import time
-
-from typing import Tuple, List
-
-from vision_processing import NetworkCommunication, ReferencePoint, Pose
+from typing import List
+from vision_processing import NetworkCommunication, Pose, DynamicObject
 
 
 class TestNetworkCommunication(NetworkCommunication):
     def __init__(self):
         super().__init__()
-        self.pose = None
-        self.end_time = time.time() + 150
 
-    def set_apriltag_pose_data(self, apriltag_array: List[ReferencePoint]) -> None:
-        average_pose = Pose.average_poses([tag.robot_pose for tag in apriltag_array])
-        self.pose = average_pose
+    def send_objects(self, objs: List[DynamicObject]):
+        print("\nObject Information")
+        print(f"Name: {[obj.object_name for obj in objs]}")
+        print(f"xPos: {[obj.absolute_coordinates[0] for obj in objs]}")
+        print(f"yPos: {[obj.absolute_coordinates[1] for obj in objs]}")
 
-    def get_kinematics(self) -> Tuple[float, float, float]:
-        """
-        Gets current kinematics from robot
-        @return: Kinematics from robot [xVelocity, yVelocity, rotVelocity]
-        """
-        return 0, 0, 0
-
-    def get_pose(self) -> Pose:
-        """
-        Gets the calculated pose of the robot
-        @return: Pose of robot
-        """
-        return self.pose
-
-    def get_robot_mode(self) -> str:
-        """
-        Gets the mode input by the driver
-        @return: String describing mode two options are "Manual" or "Auto"
-        """
-        return "Auto"
-
-    def set_robot_output(self, kinematics: Tuple[float, float, float]) -> None:
-        """
-        Sets the output of the robot
-        @param kinematics: A tuple representing robot movement [xVelocity, yVelocity, rotVelocity]
-        """
-        print(f"Robot Output:\nxVel - {kinematics[0]}\nyVel - {kinematics[1]}\nrotVel - {kinematics[2]}")
-
-    def get_team_color(self) -> str:
-        """
-        Gets the team color of the robot
-        @return: Team color: "Red" or "Blue", the default will be red
-        """
-        return "Red"
-
-    def get_game_time(self) -> float:
-        """
-        Gets the current time of the game.
-        @return: Current time into match (seconds), this value will be counting up
-        """
-        return self.end_time - time.time()
+    def send_pose(self, pose: Pose):
+        print("\nRobot Position")
+        print(f"xPos: {pose.x}")
+        print(f"yPos: {pose.y}")
+        print(f"rPos: {pose.rot}")
+        print(f"Count: {self._counter.next()}")
